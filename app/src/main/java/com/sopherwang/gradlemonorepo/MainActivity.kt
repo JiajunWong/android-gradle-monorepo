f89c983198d390aba78bd3f336fa6fc68b062974
Jiajun Wang <sopherwang@gmail.com>
2020-06-05 16:20:29 -0700

    init project

---
+++ app/src/main/java/com/sopherwang/gradlemonorepo/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.sopherwang.gradlemonorepo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
