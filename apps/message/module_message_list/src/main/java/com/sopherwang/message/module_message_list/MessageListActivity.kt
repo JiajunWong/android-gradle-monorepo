72f7f27bb1e0510c0b263409954b9e45f53d222d
Jiajun Wang <sopherwang@gmail.com>
2020-10-09 17:29:16 -0700

    Init MessageListFragment

--- apps/message/module_message_list/src/main/java/com/sopherwang/message/module_message_list/MessageListActivity.kt
+++ apps/message/module_message_list/src/main/java/com/sopherwang/message/module_message_list/MessageListActivity.kt
@@ -2,6 +2,7 @@ package com.sopherwang.message.module_message_list
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentTransaction
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -10,5 +11,9 @@ class MessageListActivity :AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val fragmentTransaction: FragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fragment_container, MessageListFragment())
+        fragmentTransaction.commit()
     }
 }
