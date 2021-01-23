30066548d5860397c3d4d4c192d6f0cb347cb8e2
Jiajun Wang <sopherwang@gmail.com>
2021-01-23 14:09:24 -0800

    Create home/content api

--- apps/mall/features/tab_main/src/main/java/com/sopherwang/mall/features/tab_main/MainFragment.kt
+++ apps/mall/features/tab_main/src/main/java/com/sopherwang/mall/features/tab_main/MainFragment.kt
@@ -1,12 +1,22 @@
 package com.sopherwang.mall.features.tab_main
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.sopherwang.mall.libraries.network.ApiStores
+import dagger.hilt.android.AndroidEntryPoint
+import io.reactivex.android.schedulers.AndroidSchedulers.mainThread
+import io.reactivex.schedulers.Schedulers.io
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainFragment : Fragment() {
+
+    @Inject
+    lateinit var apiStores: ApiStores
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -14,5 +24,20 @@ class MainFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        apiStores.getHomeContent()
+            .subscribeOn(io())
+            .observeOn(mainThread())
+            .subscribe {
+                if (it.code == 200) {
+                    Log.d("Jiajun", "Success: $it.data")
+                } else {
+                    Log.d("Jiajun", "Failed")
+                }
+            }
     }
 }
--- apps/mall/libraries/network/src/main/java/com/sopherwang/mall/libraries/network/ApiStores.kt
+++ apps/mall/libraries/network/src/main/java/com/sopherwang/mall/libraries/network/ApiStores.kt
@@ -1,4 +1,10 @@
 package com.sopherwang.mall.libraries.network
 
-interface ApiStores {
+import com.sopherwang.mall.libraries.network.models.HomeContentResponse
+import io.reactivex.Observable
+import retrofit2.http.GET
+
+public interface ApiStores {
+    @GET("home/content")
+    abstract fun getHomeContent(): Observable<HomeContentResponse>
 }
