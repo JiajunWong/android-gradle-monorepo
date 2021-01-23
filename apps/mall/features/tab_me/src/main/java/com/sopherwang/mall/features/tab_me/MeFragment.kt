9c5cd4fab0add9410421dcc7c5a340ff6586e59b
Jiajun Wang <sopherwang@gmail.com>
2021-01-22 21:58:01 -0800

    Create tab_me module

---
+++ apps/mall/features/tab_me/src/main/java/com/sopherwang/mall/features/tab_me/MeFragment.kt
@@ -0,0 +1,17 @@
+package com.sopherwang.mall.features.tab_me
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class MeFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_me, container, false)
+    }
+}
