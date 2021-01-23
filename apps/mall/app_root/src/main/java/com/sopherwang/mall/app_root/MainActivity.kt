917957ec7b778aba28bd1b32a837048557cd576c
Jiajun Wang <sopherwang@gmail.com>
2021-01-22 21:25:49 -0800

    Create bottom nav in main page

--- apps/mall/app_root/src/main/java/com/sopherwang/mall/app_root/MainActivity.kt
+++ apps/mall/app_root/src/main/java/com/sopherwang/mall/app_root/MainActivity.kt
@@ -2,10 +2,36 @@ package com.sopherwang.mall.app_root
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupBottomNavView()
+    }
+
+    private fun setupBottomNavView() {
+        val bottomNavigationView: BottomNavigationView = findViewById(R.id.main_page_bottom_navigation)
+        bottomNavigationView.setOnNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.bottom_nav_tab_main -> {
+                    return@setOnNavigationItemSelectedListener true
+                }
+                R.id.bottom_nav_tab_category -> {
+                    return@setOnNavigationItemSelectedListener true
+                }
+                R.id.bottom_nav_tab_subject -> {
+                    return@setOnNavigationItemSelectedListener true
+                }
+                R.id.bottom_nav_tab_me -> {
+                    return@setOnNavigationItemSelectedListener true
+                }
+                else -> {
+                    return@setOnNavigationItemSelectedListener false
+                }
+            }
+        }
     }
 }
