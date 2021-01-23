9d3c6e0ab9f56af86c566daac4ff51258aee509d
Jiajun Wang <sopherwang@gmail.com>
2021-01-22 21:47:12 -0800

    Create tab_category module

--- apps/mall/features/tab_category/src/main/java/com/sopherwang/mall/features/tab_category/CategoryFragment.kt
+++ apps/mall/features/tab_category/src/main/java/com/sopherwang/mall/features/tab_category/CategoryFragment.kt
@@ -1,4 +1,4 @@
-package com.sopherwang.mall.features.tab_main
+package com.sopherwang.mall.features.tab_category
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -6,13 +6,13 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 
-class MainFragment : Fragment() {
+class CategoryFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_main, container, false)
+        return inflater.inflate(R.layout.fragment_category, container, false);
     }
 }
