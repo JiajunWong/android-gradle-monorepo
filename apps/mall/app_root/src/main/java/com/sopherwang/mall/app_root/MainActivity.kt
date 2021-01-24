2fd7cc42656340d765957d1a7b82b82aaffd9d7c
Jiajun Wang <sopherwang@gmail.com>
2021-01-23 17:23:59 -0800

    Fix the viewpager scroll issue

--- apps/mall/app_root/src/main/java/com/sopherwang/mall/app_root/MainActivity.kt
+++ apps/mall/app_root/src/main/java/com/sopherwang/mall/app_root/MainActivity.kt
@@ -3,6 +3,7 @@ package com.sopherwang.mall.app_root
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.recyclerview.widget.RecyclerView
 import androidx.viewpager2.widget.ViewPager2
 import androidx.viewpager2.widget.ViewPager2.OnPageChangeCallback
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -49,14 +50,27 @@ class MainActivity : AppCompatActivity() {
 
     private fun setupViewPager() {
         viewPager = findViewById(R.id.main_page_view_pager)
+        viewPager.reduceDragSensitivity()
         with(viewPager) {
             adapter = MainPageViewPagerAdapter(supportFragmentManager, lifecycle)
-            registerOnPageChangeCallback(object: OnPageChangeCallback() {
+            registerOnPageChangeCallback(object : OnPageChangeCallback() {
                 override fun onPageSelected(position: Int) {
                     super.onPageSelected(position)
                     bottomNavigationView.menu.getItem(position).isChecked = true
                 }
             })
         }
+    }
+
+    //https://al-e-shevelev.medium.com/how-to-reduce-scroll-sensitivity-of-viewpager2-widget-87797ad02414
+    private fun ViewPager2.reduceDragSensitivity() {
+        val recyclerViewField = ViewPager2::class.java.getDeclaredField("mRecyclerView")
+        recyclerViewField.isAccessible = true
+        val recyclerView = recyclerViewField.get(this) as RecyclerView
+
+        val touchSlopField = RecyclerView::class.java.getDeclaredField("mTouchSlop")
+        touchSlopField.isAccessible = true
+        val touchSlop = touchSlopField.get(recyclerView) as Int
+        touchSlopField.set(recyclerView, touchSlop * 8)       // "8" was obtained experimentally
     }
 }
