37fddc352baca042db0a5814df64ad81263da0da
Jiajun Wang <sopherwang@gmail.com>
2021-01-31 16:57:12 -0800

    Create network lib module

--- apps/mall/app_root/src/main/java/com/sopherwang/mall/app_root/MallApplication.kt
+++ apps/mall/app_root/src/main/java/com/sopherwang/mall/app_root/MallApplication.kt
@@ -2,7 +2,15 @@ package com.sopherwang.mall.app_root
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 @HiltAndroidApp
 class MallApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+    }
 }
